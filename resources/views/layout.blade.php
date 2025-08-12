17b8baf3272abbbb8c79cf49bf01390752e25a13
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-08-12 14:54:32 +0000

    change html title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Wizard Form</title>
+    <title>Manticore Search</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body class="bg-gray-100">
